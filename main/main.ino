2ac235b36faf4ed59693e88374d4177c64482140
Burak Aktas <burak.aktas@spirohome.io>
2019-01-29 21:25:03 +0300

    Add lightLED function. Add ledIndex Work on serial input

--- main/main.ino
+++ main/main.ino
@@ -1,19 +1,43 @@
 #include <FastLED.h>
-#define NUM_LEDS 120
+#define NUM_LEDS 14
 #define DATA_PIN 6
 
 CRGB leds[NUM_LEDS];
+int currentDayIndex = 0;
+char colorInput;
+boolean newData = false;
+
+
 
 void setup() { 
+ Serial.begin(9600);
+ Serial.println("<Arduino is ready>");
+        
        FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
    }
 
 void loop() {
-  for(int dot = 0; dot < NUM_LEDS; dot++) { 
-      leds[dot] = CRGB::Blue;
-      FastLED.show();
-      // clear this led for the next time around the loop
-      leds[dot] = CRGB::Red;
-      delay(10);
+recvOneChar();
+ showNewData();
+ lightLED(2,0,0,32);
   }
+  
+void lightLED (int ledIndex, int R, int G, int B){
+leds[ledIndex].setRGB( R, G, B);
+FastLED.show(); 
+}
+
+void recvOneChar() {
+ if (Serial.available() > 0) {
+ colorInput = Serial.read();
+ newData = true;
+ }
+}
+
+void showNewData() {
+ if (newData == true) {
+ Serial.print("This just in ... ");
+ Serial.println(colorInput);
+ newData = false;
+ }
 }

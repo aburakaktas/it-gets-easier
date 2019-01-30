27caa51fd5a722a8357cc5c8c804ddfcaa260414
Burak Aktas <burak.aktas@spirohome.io>
2019-01-30 22:27:24 +0300

    Add fadeLED function with reverse capability

--- main/main.ino
+++ main/main.ino
@@ -1,43 +1,61 @@
 #include <FastLED.h>
-#define NUM_LEDS 14
+#define NUM_LEDS 1
 #define DATA_PIN 6
 
 CRGB leds[NUM_LEDS];
 int currentDayIndex = 0;
 char colorInput;
 boolean newData = false;
+int index = 0;
+int hue = 0;
+int counter = 0;
+int reverseCheck = 0;
 
+//union {
+//    struct {
+//        unsigned int is_static: 1;
+//        unsigned int is_extern: 1;
+//        unsigned int is_auto: 1;
+//    };
+//    unsigned int flags;
+//};
 
 
 void setup() { 
- Serial.begin(9600);
- Serial.println("<Arduino is ready>");
-        
-       FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
    }
 
 void loop() {
-recvOneChar();
- showNewData();
- lightLED(2,0,0,32);
+fadeLED(index,0,0,0);
   }
+  
   
 void lightLED (int ledIndex, int R, int G, int B){
 leds[ledIndex].setRGB( R, G, B);
 FastLED.show(); 
 }
 
-void recvOneChar() {
- if (Serial.available() > 0) {
- colorInput = Serial.read();
- newData = true;
- }
+void fadeLED (int ledIndex, int R, int G, int B){
+while (reverseCheck == 0) {
+   R = counter;
+   G = 255-counter;
+   lightLED(index,R,G,0);
+   counter++;
+   if(counter == 255){
+     reverseCheck= 1;
+   };
+   delay(20);
+}
+while (reverseCheck == 1) {
+   R = counter;
+   G = 255-counter;
+   lightLED(index,R,G,0);
+   counter--;
+   if(counter == 0){
+     reverseCheck= 0;
+   };
+   delay(20);
+  
 }
 
-void showNewData() {
- if (newData == true) {
- Serial.print("This just in ... ");
- Serial.println(colorInput);
- newData = false;
- }
 }
